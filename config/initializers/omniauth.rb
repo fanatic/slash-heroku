5a07fbf81c7f4600e62af06c914522fec700800a
Corey Donohoe <atmos@atmos.org>
2016-03-21 16:35:17 -0700

    try relaxing scopes on oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_OAUTH_ID"], ENV["GITHUB_OAUTH_SECRET"], scope: "user,repo"
+  provider :github, ENV["GITHUB_OAUTH_ID"], ENV["GITHUB_OAUTH_SECRET"], scope: "repo_deployemtn,repo:status"
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :heroku, ENV["HEROKU_OAUTH_ID"], ENV["HEROKU_OAUTH_SECRET"], scope: "global", fetch_info: true
+  provider :heroku, ENV["HEROKU_OAUTH_ID"], ENV["HEROKU_OAUTH_SECRET"], scope: "write-restricted", fetch_info: true
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
