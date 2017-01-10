9ecd6e58088903cfda76498b4f26926303799ab2
Corey Donohoe <atmos@atmos.org>
2017-01-10 12:55:51 -0800

    bring back wcid basically

--- app/models/heroku_commands/pipelines.rb
+++ app/models/heroku_commands/pipelines.rb
@@ -10,28 +10,31 @@ module HerokuCommands
 
     def self.help_documentation
       [
+        "pipelines - View available pipelines.",
         "pipelines:info -a APP - View detailed information for a pipeline."
       ]
     end
 
     def run
       @response = run_on_subtask
-    end
-
-    def run_on_subtask
-      case subtask
-      when "info", "default"
-        pipeline_info
-      else
-        response_for("pipeline:#{subtask} is currently unimplemented.")
-      end
-    rescue Escobar::GitHub::RepoNotFound => e
-      response_for("You're not authenticated with GitHub. " \
-                   "<#{command.github_auth_url}|Fix that>.")
     rescue StandardError => e
       raise e if Rails.env.test?
       Raven.capture_exception(e)
       response_for("Unable to fetch pipeline info for #{application}.")
+    end
+
+    def run_on_subtask
+      case subtask
+      when "info"
+        pipeline_info
+      when "list", "default"
+        response_for("You can deploy: #{pipelines.app_names.join(', ')}.")
+      else
+        response_for("pipeline:#{subtask} is currently unimplemented.")
+      end
+    rescue Escobar::GitHub::RepoNotFound
+      response_for("You're not authenticated with GitHub. " \
+                   "<#{command.github_auth_url}|Fix that>.")
     end
   end
 end
