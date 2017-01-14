3fee3e7e31b330e6725a77ec0e75125ecc44f9a4
Corey Donohoe <atmos@atmos.org>
2017-01-13 19:49:07 -0800

    try more logging to find the error string

--- config/initializers/ext.rb
+++ config/initializers/ext.rb
@@ -10,8 +10,32 @@ module Escobar
             version_description: "#{pipeline.github_repository}:#{sha}"
           }
         }
-        Rails.logger.info body
+        Rails.logger.info action: :create_heroku_build, body: body
         app.client.heroku.post("/apps/#{app.name}/builds", body)
+      end
+    end
+  end
+  module GitHub
+    class Client
+      def create_deployment(options)
+        body = {
+          ref: options[:ref] || "master",
+          task: "deploy",
+          auto_merge: false,
+          required_contexts: options[:required_contexts] || [],
+          payload: options[:payload] || {},
+          environment: options[:environment] || "staging",
+          description: "Shipped from chat with slash-heroku"
+        }
+        Rails.logger.info action: :create_githun_deployment, body: body
+
+        post("/repos/#{name_with_owner}/deployments", body)
+      end
+
+      def create_deployment_status(url, payload)
+        uri = URI.parse(url)
+        Rails.logger.info action: :create_github_deployment_status, body: payload
+        post("#{uri.path}/statuses", payload)
       end
     end
   end
