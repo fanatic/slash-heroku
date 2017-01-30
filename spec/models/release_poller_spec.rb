38080feb7d815f475caeb8c85f041dec029f5224
Yannick Schutz <yannick.schutz@gmail.com>
2017-01-30 15:01:57 +0100

    Extract stub helpers in ReleasePollerSpec

--- spec/models/release_poller_spec.rb
+++ spec/models/release_poller_spec.rb
@@ -26,58 +26,79 @@ RSpec.describe ReleasePoller, type: :model do
     }
   end
 
-  # rubocop:disable Metrics/LineLength
-  it "successfully polls a release" do
+  def stub_pipelines_info
     response_info = fixture_data("api.heroku.com/pipelines/info")
     stub_request(:get, "https://api.heroku.com/pipelines")
       .with(headers: default_heroku_headers(user.heroku_token))
       .to_return(status: 200, body: response_info, headers: {})
+  end
 
-    response_info = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/builds/#{release_args[:build_id]}")
-    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/builds/#{release_args[:build_id]}")
+  def stub_build_with_id(build_id)
+    response_info = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/builds/#{build_id}") # rubocop:disable Metrics/LineLength
+    stub_build_with_id_and_response(build_id, response_info)
+  end
+
+  def stub_pending_build_with_id(build_id)
+    response_info = fixture_data("api.heroku.com/builds/pending")
+    stub_build_with_id_and_response(build_id, response_info)
+  end
+
+  def stub_completed_build_without_release_with_id(build_id)
+    response_info = fixture_data("api.heroku.com/builds/completed_without_release") # rubocop:disable Metrics/LineLength
+    stub_build_with_id_and_response(build_id, response_info)
+  end
+
+  def stub_build_with_id_and_response(build_id, response_info)
+    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/builds/#{build_id}") # rubocop:disable Metrics/LineLength
       .with(headers: default_heroku_headers(user.heroku_token))
       .to_return(status: 200, body: response_info, headers: {})
+  end
 
-    response_info = fixture_data("kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository")
-    stub_request(:get, "https://kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository")
+  def stub_kolkrabbi_repository
+    response_info = fixture_data("kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository") # rubocop:disable Metrics/LineLength
+    stub_request(:get, "https://kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository") # rubocop:disable Metrics/LineLength
       .to_return(status: 200, body: response_info)
+  end
 
-    response_info = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/releases/#{release_args[:release_id]}")
-    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/releases/#{release_args[:release_id]}")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
-
+  def stub_status_creation(deployment_url)
     stub_request(:post, "#{deployment_url}/statuses")
       .to_return(status: 200, body: {}.to_json, headers: {})
+  end
+
+  def stub_release_with_id(release_id)
+    response_info = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/releases/#{release_id}") # rubocop:disable Metrics/LineLength
+    stub_release_with_id_and_response(release_id, response_info)
+  end
+
+  def stub_pending_release_with_id(release_id)
+    response_info = fixture_data("api.heroku.com/releases/pending")
+    stub_release_with_id_and_response(release_id, response_info)
+  end
+
+  def stub_release_with_id_and_response(release_id, response_info)
+    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/releases/#{release_id}") # rubocop:disable Metrics/LineLength
+      .with(headers: default_heroku_headers(user.heroku_token))
+      .to_return(status: 200, body: response_info, headers: {})
+  end
+
+  it "successfully polls a release" do
+    stub_pipelines_info
+    stub_build_with_id(release_args[:build_id])
+    stub_kolkrabbi_repository
+    stub_release_with_id(release_args[:release_id])
+    stub_status_creation(deployment_url)
 
     poller = ReleasePoller.run(release_args)
     expect(poller.release.status).to eql("succeeded")
   end
-  # rubocop:enable Metrics/LineLength
 
-  # rubocop:disable Metrics/LineLength
   it "retry later if release is pending" do
-    response_info = fixture_data("api.heroku.com/pipelines/info")
-    stub_request(:get, "https://api.heroku.com/pipelines")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
+    stub_pipelines_info
+    stub_build_with_id(release_args[:build_id])
+    stub_kolkrabbi_repository
+    stub_pending_release_with_id(release_args[:release_id])
 
-    response_info = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/builds/#{release_args[:build_id]}")
-    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/builds/#{release_args[:build_id]}")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
-
-    response_info = fixture_data("kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository")
-    stub_request(:get, "https://kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository")
-      .to_return(status: 200, body: response_info)
-
-    response_info = fixture_data("api.heroku.com/releases/pending")
-    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/releases/#{release_args[:release_id]}")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
-
-    status_update = stub_request(:post, "#{deployment_url}/statuses")
-                    .to_return(status: 200, body: {}.to_json, headers: {})
+    status_update = stub_status_creation(deployment_url)
 
     poller = nil
     ActiveJob::Base.queue_adapter = :test
@@ -87,31 +108,13 @@ RSpec.describe ReleasePoller, type: :model do
     expect(status_update).to_not have_been_requested
     expect(poller.release.status).to eql("pending")
   end
-  # rubocop:enable Metrics/LineLength
 
-  # rubocop:disable Metrics/LineLength
   it "unlocks the app if the release succeeds" do
-    response_info = fixture_data("api.heroku.com/pipelines/info")
-    stub_request(:get, "https://api.heroku.com/pipelines")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
-
-    response_info = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/builds/#{release_args[:build_id]}")
-    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/builds/#{release_args[:build_id]}")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
-
-    response_info = fixture_data("kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository")
-    stub_request(:get, "https://kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository")
-      .to_return(status: 200, body: response_info)
-
-    response_info = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/releases/#{release_args[:release_id]}")
-    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/releases/#{release_args[:release_id]}")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
-
-    stub_request(:post, "#{deployment_url}/statuses")
-      .to_return(status: 200, body: {}.to_json, headers: {})
+    stub_pipelines_info
+    stub_build_with_id(release_args[:build_id])
+    stub_kolkrabbi_repository
+    stub_release_with_id(release_args[:release_id])
+    stub_status_creation(deployment_url)
 
     poller = ReleasePoller.new(release_args)
     lock = Lock.new(poller.release.app.cache_key)
@@ -119,31 +122,12 @@ RSpec.describe ReleasePoller, type: :model do
     poller.run
     expect(lock).to_not be_locked
   end
-  # rubocop:enable Metrics/LineLength
 
-  # rubocop:disable Metrics/LineLength
   it "retry later if release is pending" do
-    response_info = fixture_data("api.heroku.com/pipelines/info")
-    stub_request(:get, "https://api.heroku.com/pipelines")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
-
-    response_info = fixture_data("api.heroku.com/apps/b0deddbf-cf56-48e4-8c3a-3ea143be2333/builds/#{release_args[:build_id]}")
-    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/builds/#{release_args[:build_id]}")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
-
-    response_info = fixture_data("kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository")
-    stub_request(:get, "https://kolkrabbi.com/pipelines/4c18c922-6eee-451c-b7c6-c76278652ccc/repository")
-      .to_return(status: 200, body: response_info)
-
-    response_info = fixture_data("api.heroku.com/releases/pending")
-    stub_request(:get, "https://api.heroku.com/apps/slash-h-production/releases/#{release_args[:release_id]}")
-      .with(headers: default_heroku_headers(user.heroku_token))
-      .to_return(status: 200, body: response_info, headers: {})
-
-    stub_request(:post, "#{deployment_url}/statuses")
-      .to_return(status: 200, body: {}.to_json, headers: {})
+    stub_pipelines_info
+    stub_build_with_id(release_args[:build_id])
+    stub_kolkrabbi_repository
+    stub_pending_release_with_id(release_args[:release_id])
 
     poller = ReleasePoller.new(release_args)
     lock = Lock.new(poller.release.app.cache_key)
@@ -151,5 +135,4 @@ RSpec.describe ReleasePoller, type: :model do
     poller.run
     expect(lock).to be_locked
   end
-  # rubocop:enable Metrics/LineLength
 end
