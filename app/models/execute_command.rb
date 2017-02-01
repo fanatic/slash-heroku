eb94b3e1a16baad8c185442c8ec3d533a22225d1
Stella Cotton <stella@stellacotton.com>
2017-01-31 20:17:07 -0800

    Check auth earlier for pipeline and deploy

--- app/models/execute_command.rb
+++ app/models/execute_command.rb
@@ -1,34 +1,64 @@
 # Generates command body and posts it to slack
 class ExecuteCommand
-  attr_reader :command
+  attr_reader :command, :task
+
+  REQUIRES_AUTHENTICATION = %w{pipeline pipelines deploy}.freeze
 
   def self.for(command)
-    new(command).run
+    new(command).post_to_slack
   end
 
   def initialize(command)
     @command = command
+    @task = command.task
   end
 
-  def run
+  def response
     handler.run
-    SlackPostback.for(handler.response, command.response_url)
+    handler.response
+  end
+
+  def post_to_slack
+    SlackPostback.for(response, command.response_url)
   end
 
   def handler
-    @handler ||= case command.task
-                 when "deploy"
-                   HerokuCommands::Deploy.new(command)
-                 when "login"
-                   HerokuCommands::Login.new(command)
-                 when "logout"
-                   HerokuCommands::Logout.new(command)
-                 when "pipeline", "pipelines"
-                   HerokuCommands::Pipelines.new(command)
-                 when "releases"
-                   HerokuCommands::Releases.new(command)
-                 else # when "help"
-                   HerokuCommands::Help.new(command)
-                 end
+    @handler ||=
+      if logging_in || needs_authentication
+        HerokuCommands::Login.new(command)
+      else
+        case task
+        when "deploy"
+          HerokuCommands::Deploy.new(command)
+        when "logout"
+          HerokuCommands::Logout.new(command)
+        when "pipeline", "pipelines"
+          HerokuCommands::Pipelines.new(command)
+        when "releases"
+          HerokuCommands::Releases.new(command)
+        else # when "help"
+          HerokuCommands::Help.new(command)
+        end
+      end
+  end
+
+  def needs_authentication
+    REQUIRES_AUTHENTICATION.include?(task) && not_setup?
+  end
+
+  def logging_in
+    command.task == "login"
+  end
+
+  def not_setup?
+    !command.user.heroku_configured? || !command.user.github_configured?
+  end
+
+  def response_for(text)
+    {
+      attachments: [
+        { text: text, response_type: "in_channel" }
+      ]
+    }
   end
 end
--- spec/models/execute_command_spec.rb
+++ spec/models/execute_command_spec.rb
@@ -4,27 +4,93 @@ RSpec.describe ExecuteCommand, type: :model do
   include Helpers::Command::Pipelines
 
   describe "Pipelines command" do
+    let(:command) { command_for("pipelines") }
+
+    it "checks to make sure you're authenticated with heroku" do
+      command.user.heroku_token = nil
+      command.user.save
+
+      stub_please_sign_into_heroku
+
+      ExecuteCommand.for(command)
+
+      expect(stub_please_sign_into_heroku).to have_been_requested
+    end
+
+    it "checks to make sure you're authenticated with Github" do
+      command.user.github_token = nil
+      command.user.save
+
+      stub_please_sign_into_github
+
+      ExecuteCommand.for(command)
+
+      expect(stub_please_sign_into_github).to have_been_requested
+    end
+
     it "lists available pipelines" do
-      command = command_for("pipelines")
       command.user.github_token = SecureRandom.hex(24)
+      command.user.heroku_token = SecureRandom.hex(24)
       command.user.save
 
       stub_pipelines_command(command.user.heroku_token)
 
-      slack_body = {
-        attachments:
-          [
-            {
-              text: "You can deploy: hubot, slash-heroku."
-            }
-          ]
-      }.to_json
-
+      message = "You can deploy: hubot, slash-heroku."
+      slack_body = slack_body(message)
       stub = stub_slack_request(slack_body)
 
       ExecuteCommand.for(command)
 
       expect(stub).to have_been_requested
     end
+  end
+
+  describe "Deploy command" do
+    let(:command) { command_for("deploy hubot") }
+    it "checks to make sure you're authenticated with heroku" do
+      command.user.heroku_token = nil
+      command.user.save
+
+      stub_please_sign_into_heroku
+
+      ExecuteCommand.for(command)
+
+      expect(stub_please_sign_into_heroku).to have_been_requested
+    end
+
+    it "checks to make sure you're authenticated with Github" do
+      command.user.github_token = nil
+      command.user.save
+
+      stub_please_sign_into_github
+
+      ExecuteCommand.for(command)
+
+      expect(stub_please_sign_into_github).to have_been_requested
+    end
+  end
+
+  def slack_body(message)
+    {
+      attachments:
+        [
+          {
+            text: message
+          }
+        ]
+    }.to_json
+  end
+
+  def stub_please_sign_into_heroku
+    message = "Please <#{command.slack_auth_url}|sign in to Heroku>."
+    slack_body = slack_body(message)
+    stub_slack_request(slack_body)
+  end
+
+  def stub_please_sign_into_github
+    message = "You're not authenticated with GitHub yet. " \
+                "<#{command.github_auth_url}|Fix that>."
+    slack_body = slack_body(message)
+    stub_slack_request(slack_body)
   end
 end
