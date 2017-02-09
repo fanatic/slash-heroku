6815855c6b29d2428733dbd0ce11c2d9e602c07d
Yannick Schutz <yannick.schutz@gmail.com>
2017-02-09 19:04:55 +0100

    Extract the helpers to see clearly the goal in deploy_spec

--- spec/support/helpers/command/deploy.rb
+++ spec/support/helpers/command/deploy.rb
@@ -111,6 +111,162 @@ module Helpers
           .to_return(status: 200, body: response_info, headers: {})
       end
 
+      # NEW Helpers
+      def stub_pipelines(pipelines)
+        pipelines = pipelines.is_a?(Array) ? pipelines : [pipelines]
+        stub_json_request(:get,
+                          "https://api.heroku.com/pipelines",
+                          pipelines.to_json)
+      end
+
+      def stub_couplings(pipeline_id, apps, stage = "production")
+        apps = apps.is_a?(Array) ? apps : [apps]
+        couplings = apps.map do |app|
+          { stage: stage, app: { id: app[:id] } }
+        end
+        url = "https://api.heroku.com/pipelines/#{pipeline_id}/pipeline-couplings"
+        stub_json_request(:get,
+                          url,
+                          couplings.to_json)
+      end
+
+      def stub_2fa_check(app_id, args = {})
+        locked = args[:locked]
+        status = locked ? 403 : 200
+        body = locked ? { id: "two_factor" } : {}
+        stub_json_request(:get,
+                          "https://api.heroku.com/apps/#{app_id}/config-vars",
+                          body.to_json,
+                          status)
+      end
+
+      def stub_heroku_app(id, name)
+        app = { id: id, name: name }
+        stub_json_request(:get, "https://api.heroku.com/apps/#{id}", app.to_json)
+      end
+
+      def stub_mapping_pipeline_repository(pipeline_id, repo_name_with_owner)
+        url = "https://kolkrabbi.com/pipelines/#{pipeline_id}/repository"
+        payload = { repository: { name: repo_name_with_owner } }
+        stub_json_request(:get, url, payload.to_json)
+      end
+
+      def stub_repository(name_with_owner, default_branch = "master")
+        stub_json_request(:get,
+                          "https://api.github.com/repos/#{name_with_owner}",
+                          { default_branch: default_branch }.to_json)
+      end
+
+      def stub_required_contexts(name_with_owner, branch = "master")
+        url = "https://api.github.com/repos/#{name_with_owner}/branches/#{branch}"
+        stub_json_request(:get, url, {}.to_json)
+      end
+
+      def stub_deployment_conflict(repo)
+        payload = { message: "Conflict: Commit status checks failed for master." }
+        stub_json_request(:post,
+                          "https://api.github.com/repos/#{repo}/deployments",
+                          payload.to_json,
+                          409)
+      end
+
+      def stub_deployment_status(repo)
+        stub_json_request(:post,
+                          "https://api.github.com/repos/#{repo}/deployments",
+                          {}.to_json,
+                          200)
+      end
+
+      def stub_missing_required_commit_status_flow(pipeline_name, app_name, repo)
+        pipeline =  { id: SecureRandom.uuid, name: pipeline_name }
+        app = { id: SecureRandom.uuid, name: app_name }
+
+        stub_pipelines(pipeline)
+        stub_couplings(pipeline[:id], app)
+        stub_2fa_check(app[:id])
+        stub_heroku_app(app[:id], app[:name])
+
+        stub_mapping_pipeline_repository(pipeline[:id], repo)
+
+        stub_repository(repo)
+        stub_required_contexts(repo)
+
+        stub_deployment_conflict(repo)
+      end
+
+      def stub_2fa_locked_app_flow(pipeline_name, app_name)
+        pipeline =  { id: SecureRandom.uuid, name: pipeline_name }
+        app = { id: SecureRandom.uuid, name: app_name }
+
+        stub_pipelines(pipeline)
+        stub_couplings(pipeline[:id], app)
+        stub_heroku_app(app[:id], app[:name])
+        stub_2fa_check(app[:id], locked: true)
+      end
+
+      def stub_locked_deployment_flow(pipeline_name, app_name)
+        pipeline =  { id: SecureRandom.uuid, name: pipeline_name }
+        app = { id: SecureRandom.uuid, name: app_name }
+
+        stub_pipelines(pipeline)
+        stub_couplings(pipeline[:id], app)
+        stub_heroku_app(app[:id], app[:name])
+        stub_2fa_check(app[:id])
+
+        # Fake the lock
+        Lock.new("escobar-app-#{app[:id]}").lock
+      end
+
+      def stub_multiple_apps_in_stage_flow(args = {})
+        pipeline_name   = args[:pipeline_name]
+        app_names       = args[:app_names]
+        chosen_app_name = args[:chosen_app_name]
+        repo            = args[:repo] || "atmos/hubot"
+
+        pipeline = { id: SecureRandom.uuid, name: pipeline_name }
+        apps = stubbed_apps_hash_from_names(app_names)
+
+        stub_pipelines(pipeline)
+        stub_couplings(pipeline[:id], apps)
+
+        return unless chosen_app_name
+        chosen_app = apps.detect { |app| app[:name] == chosen_app_name }
+        stub_chosen_app(chosen_app, repo)
+      end
+
+      def stubbed_apps_hash_from_names(app_names)
+        app_names.map do |app_name|
+          id = SecureRandom.uuid
+          stub_heroku_app(id, app_name)
+          { id: id, name: app_name }
+        end
+      end
+
+      def stub_chosen_app(chosen_app, repo)
+        stub_2fa_check(chosen_app[:id])
+        stub_mapping_pipeline_repository(pipeline[:id], repo)
+
+        stub_repository(repo)
+        stub_required_contexts(repo)
+        stub_deployment_status(repo)
+      end
+
+      def stub_successful_deployment_flow(pipeline_name)
+        pipeline =  { id: SecureRandom.uuid, name: pipeline_name }
+        app = { id: SecureRandom.uuid, name: pipeline_name }
+        repo = "heroku/#{pipeline_name}"
+
+        stub_pipelines(pipeline)
+        stub_couplings(pipeline[:id], app)
+        stub_heroku_app(app[:id], app[:name])
+        stub_2fa_check(app[:id])
+        stub_mapping_pipeline_repository(pipeline[:id], repo)
+
+        stub_repository(repo)
+        stub_required_contexts(repo)
+        stub_deployment_status(repo)
+      end
+
       # rubocop:enable Metrics/LineLength
       # rubocop:enable Metrics/AbcSize
       # rubocop:enable Metrics/MethodLength
