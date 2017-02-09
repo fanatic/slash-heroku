cbd44d31b51acc797717451db6ed1fd873f7a3ff
Thomas Balthazar <tbalthazar@users.noreply.github.com>
2017-02-09 17:11:44 +0100

    Fix tests for multiple apps (#74)
    Test that an application is deployed if the pipeline has multiple apps
    and an app is specified.

--- spec/support/helpers/command/deploy.rb
+++ spec/support/helpers/command/deploy.rb
@@ -8,11 +8,18 @@ module Helpers
       APP_IDS = [
         "27bde4b5-b431-4117-9302-e533b887faaa",
         "b0deddbf-cf56-48e4-8c3a-3ea143be2333",
+        "760bc95e-8780-4c76-a688-3a4af92a3eee",
+        "860bc95e-8780-4c76-a688-3a4af92a3eee",
         "c0deddbf-cf56-48e4-8c3a-3ea143be2333"
+      ].freeze
+      REPOS = [
+        "atmos/hubot",
+        "heroku/beeper"
       ].freeze
 
       # rubocop:disable Metrics/LineLength
       # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/MethodLength
       def stub_deploy_command(heroku_token)
         stub_account_info(heroku_token)
         stub_pipeline_info(heroku_token)
@@ -27,33 +34,41 @@ module Helpers
           .with(headers: default_heroku_headers(heroku_token))
           .to_return(status: 200, body: { id: "191853f6-0635-44cc-8d97-ef8feae0e178" }.to_json, headers: {})
 
+        stub_request(:post, "https://api.heroku.com/apps/beeper-production-foo/builds")
+          .with(headers: default_heroku_headers(heroku_token))
+          .to_return(status: 200, body: { id: "191853f6-0635-44cc-8d97-ef8feae0e178" }.to_json, headers: {})
+
         PIPELINE_IDS.each do |p_id|
           response_info = fixture_data("kolkrabbi.com/pipelines/#{p_id}/repository")
           stub_request(:get, "https://kolkrabbi.com/pipelines/#{p_id}/repository")
             .to_return(status: 200, body: response_info)
         end
 
-        response_info = fixture_data("api.github.com/repos/atmos/hubot/index")
-        stub_request(:get, "https://api.github.com/repos/atmos/hubot")
-          .to_return(status: 200, body: response_info, headers: {})
-
-        response_info = fixture_data("api.github.com/repos/atmos/hubot/branches/production")
-        stub_request(:get, "https://api.github.com/repos/atmos/hubot/branches/production")
-          .to_return(status: 200, body: response_info, headers: {})
-
         sha = "27bd10a885d27ba4db2c82dd34a199b6a0a8149c"
-        response_info = fixture_data("api.github.com/repos/atmos/hubot/tarball/#{sha}")
-        stub_request(:head, "https://api.github.com/repos/atmos/hubot/tarball/#{sha}")
-          .to_return(status: 200, body: response_info, headers: { "Location" => "https://codeload.github.com/atmos/hubot/legacy.tar.gz/master" })
+        REPOS.each do |repo|
+          response_info = fixture_data("api.github.com/repos/#{repo}/index")
+          stub_request(:get, "https://api.github.com/repos/#{repo}")
+            .to_return(status: 200, body: response_info, headers: {})
 
-        url = "https://api.github.com/repos/atmos/hubot/deployments/4307227"
-        stub_request(:post, "https://api.github.com/repos/atmos/hubot/deployments")
-          .to_return(status: 200, body: { sha: sha, url: url }.to_json, headers: {})
+          response_info = fixture_data("api.github.com/repos/#{repo}/branches/production")
+          stub_request(:get, "https://api.github.com/repos/#{repo}/branches/production")
+            .to_return(status: 200, body: response_info, headers: {})
+
+          response_info = fixture_data("api.github.com/repos/#{repo}/tarball/#{sha}")
+          stub_request(:head, "https://api.github.com/repos/#{repo}/tarball/#{sha}")
+            .to_return(status: 200, body: response_info, headers: { "Location" => "https://codeload.github.com/#{repo}/legacy.tar.gz/master" })
+
+          url = "https://api.github.com/repos/#{repo}/deployments/4307227"
+          stub_request(:post, "https://api.github.com/repos/#{repo}/deployments")
+            .to_return(status: 200, body: { sha: sha, url: url }.to_json, headers: {})
+        end
       end
 
       def stub_github_status
-        stub_request(:post, "https://api.github.com/repos/atmos/hubot/deployments/4307227/statuses")
-          .to_return(status: 200, body: {}.to_json, headers: {})
+        REPOS.each do |repo|
+          stub_request(:post, "https://api.github.com/repos/#{repo}/deployments/4307227/statuses")
+            .to_return(status: 200, body: {}.to_json, headers: {})
+        end
       end
 
       def stub_app_is_not_2fa(heroku_token)
@@ -98,6 +113,7 @@ module Helpers
 
       # rubocop:enable Metrics/LineLength
       # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
