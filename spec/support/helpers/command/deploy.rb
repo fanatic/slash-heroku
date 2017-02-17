43903971deae7c98878b68346fc7b31e9b488b7a
Yannick Schutz <yannick.schutz@gmail.com>
2017-02-17 14:14:23 +0100

    Use new api helpers to shorten lines

--- spec/support/helpers/command/deploy.rb
+++ spec/support/helpers/command/deploy.rb
@@ -13,25 +13,23 @@ module Helpers
         couplings = apps.map do |app|
           { stage: stage, app: { id: app[:id] } }
         end
-        url = "https://api.heroku.com/pipelines/#{pipeline_id}/pipeline-couplings"
-        stub_json_request(:get,
-                          url,
-                          couplings.to_json)
+        path = "/pipelines/#{pipeline_id}/pipeline-couplings"
+        stub_heroku_request(:get, path, couplings.to_json)
       end
 
       def stub_2fa_check(app_id, args = {})
         locked = args[:locked]
         status = locked ? 403 : 200
         body = locked ? { id: "two_factor" } : {}
-        stub_json_request(:get,
-                          "https://api.heroku.com/apps/#{app_id}/config-vars",
-                          body.to_json,
-                          status)
+        stub_heroku_request(:get,
+                            "/apps/#{app_id}/config-vars",
+                            body.to_json,
+                            status)
       end
 
       def stub_heroku_app(id, name)
         app = { id: id, name: name }
-        stub_json_request(:get, "https://api.heroku.com/apps/#{id}", app.to_json)
+        stub_heroku_request(:get, "/apps/#{id}", app.to_json)
       end
 
       def stub_mapping_pipeline_repository(pipeline_id, repo_name_with_owner)
@@ -46,29 +44,28 @@ module Helpers
       end
 
       def stub_repository(name_with_owner, default_branch = "master")
-        stub_json_request(:get,
-                          "https://api.github.com/repos/#{name_with_owner}",
-                          { default_branch: default_branch }.to_json)
+        stub_github_request(:get,
+                            "/repos/#{name_with_owner}",
+                            { default_branch: default_branch }.to_json)
       end
 
       def stub_required_contexts(name_with_owner, branch = "master")
-        url = "https://api.github.com/repos/#{name_with_owner}/branches/#{branch}"
-        stub_json_request(:get, url, {}.to_json)
+        path = "/repos/#{name_with_owner}/branches/#{branch}"
+        stub_github_request(:get, path, {}.to_json)
       end
 
       def stub_deployment_conflict(repo)
-        payload = { message: "Conflict: Commit status checks failed for master." }
-        stub_json_request(:post,
-                          "https://api.github.com/repos/#{repo}/deployments",
-                          payload.to_json,
-                          409)
+        payload = {
+          message: "Conflict: Commit status checks failed for master."
+        }
+        stub_github_request(:post,
+                            "/repos/#{repo}/deployments",
+                            payload.to_json,
+                            409)
       end
 
       def stub_deployment_status(repo)
-        stub_json_request(:post,
-                          "https://api.github.com/repos/#{repo}/deployments",
-                          {}.to_json,
-                          200)
+        stub_github_request(:post, "/repos/#{repo}/deployments", {}.to_json)
       end
 
       def stub_pipeline_not_connected_to_github_flow(pipeline_id, pipeline_name)
@@ -81,7 +78,9 @@ module Helpers
         stub_pipeline_repository_not_found(pipeline[:id])
       end
 
-      def stub_missing_required_commit_status_flow(pipeline_name, app_name, repo)
+      def stub_missing_required_commit_status_flow(pipeline_name,
+                                                   app_name,
+                                                   repo)
         pipeline =  { id: SecureRandom.uuid, name: pipeline_name }
         app = { id: SecureRandom.uuid, name: app_name }
 
@@ -105,7 +104,6 @@ module Helpers
         stub_pipelines(pipeline)
         stub_couplings(pipeline[:id], app, environments[:available_env])
       end
-
 
       def stub_2fa_locked_app_flow(pipeline_name, app_name)
         pipeline =  { id: SecureRandom.uuid, name: pipeline_name }
@@ -140,7 +138,7 @@ module Helpers
         pipeline_name   = args[:pipeline_name]
         app_names       = args[:app_names]
         chosen_app_name = args[:chosen_app_name]
-        repo            = args[:repo] || "atmos/hubot"
+        repo            = args.fetch(:repo, "atmos/hubot")
 
         pipeline = { id: SecureRandom.uuid, name: pipeline_name }
 
@@ -153,6 +151,7 @@ module Helpers
 
         return unless chosen_app_name
         chosen_app = apps.detect { |app| app[:name] == chosen_app_name }
+
         stub_chosen_app(chosen_app, repo, pipeline)
       end
 
