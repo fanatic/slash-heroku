83f93d80345bf8b49a03faabffa51350727d2d55
Yannick Schutz <yannick.schutz@gmail.com>
2017-02-17 14:13:33 +0100

    Add api helpers for github and heroku

--- spec/support/webmock_helpers.rb
+++ spec/support/webmock_helpers.rb
@@ -9,6 +9,18 @@ module WebmockHelpers
       .to_return(status: status, body: response_body, headers: default_json_headers)
   end
 
+  def stub_heroku_request(method, path, response_body, status = 200)
+    base_url = "https://api.heroku.com"
+    stub_request(method, base_url + path)
+      .to_return(status: status, body: response_body, headers: default_json_headers)
+  end
+
+  def stub_github_request(method, path, response_body, status = 200)
+    base_url = "https://api.github.com"
+    stub_request(method, base_url + path)
+      .to_return(status: status, body: response_body, headers: default_json_headers)
+  end
+
   def stub_slack_request(body)
     stub_request(:post, "https://hooks.slack.com/commands/T123YG08V/2459573/mPdDq")
       .with(body:    body,
